552b4d37d3766d6cc85e297e8fab51a3e1c592c0
fkelfk <fkelfk09@gmail.com>
2022-07-04 19:11:29 +0900

    draft

--- owners/views.py
+++ owners/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from .models import Owner, Dog
+
+class DogsView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        ownerName = data['ownerName']
+        ownerAge = data['ownerAge']
+        ownerEmail = data['ownerEmail']
+
+        owner = Owner.objects.create(
+            name=ownerName,
+            age=ownerAge,
+            email=ownerEmail,
+
+        )
+
+        Dog.objects.create(
+            name=data['name'],
+            age=data['age'],
+            owner=owner
+        )
+
+        return JsonResponse({'message': 'created'}, status=201)
+
+    def get(self, request):
+        dogs = Dog.objects.all()
+        dogList = []
+
+        for dog in dogs:
+            dogList.append(
+                {
+                    "owner": dog.owner.name,
+                    "dog_name": dog.name,
+                    "dog_age": dog.age,
+                }
+            )
+
+        return JsonResponse({'dogList': dogList}, status=200)
+
+
+class OwnersView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        Owner.objects.create(
+            name=data['name'],
+            age=data['age'],
+            email=data['email'],
+        )
+
+        return JsonResponse({'message': 'created'}, status=201)
+
+    def get(self, request):
+        owners = Owner.objects.all()
+        ownerList = []
+
+        for owner in owners:
+            ownerList.append(
+                {
+                    "name": owner.name,
+                    "age": owner.age,
+                    "email": owner.email,
+                }
+            )
+
+        return JsonResponse({'ownerList': ownerList}, status=200)
